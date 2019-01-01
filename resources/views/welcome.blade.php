5430ccf88c14bfe296a37e65c3ec78fa3368df9b
Senthan <senthaneng@gmail.com>
2019-01-01 14:07:24 +0800

    physiosym init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="keywords" content="National University of Singapore, NUS, Singapore Universities, Physiology" />
+    <meta name="description" content="A research-intensive university with an entrepreneurial dimension, NUS is ranked consistently as one of the world's top universities. We offer the most extensive selection of academic programmes in Singapore, collaborating with leading universities worldwide to provide our students with diverse opportunities for overseas exposure." />
+    <title>Mark Mattson International Neuroscience Symposium 2019</title>
+    <link href="images/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/styleguide.css" type="text/css" />
+    <link rel="stylesheet" href="css/template.css" type="text/css" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+<div class="nus-body-container">
+
+    <div class="nus-wrapper">
+
+        <!-- HEADER -->
+        <header class="nus-header">
+            <!-- set 'microsite' to get #eee background. Otherwise, remove it to get #fff background -->
+            <div class="container">
+
+                <div class="nus-mobile-nav hidden-lg hidden-md">
+                    <ul>
+                        <li>
+                            <a class="faicon fa-search collapsed" data-toggle="collapse" data-target="#search-collapse"
+                               href="javascript:void(0)">&nbsp;</a>
+                        </li>
+                        <li>
+                            <a class="faicon fa-mobile-menu btn-toggle off-canvas-toggle" data-pos="right"
+                               data-effect="off-canvas-effect-4" data-nav="#nus-sidebar-off-canvas" href="javascript:void(0)">&nbsp;</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
+        </header>
+        <!-- //HEADER -->
+
+
+
+        <div class="container">
+
+            <div class="nus-components">
+                <div class="row">
+                    <!-- <div class="col-lg-12">
+                        <h1>10th Models of Physiology and Diseases Symposium 2018</h1>
+                        <h2>24 - 25 September 2018 (Monday - Tuesday)</h2>
+                        <h2>CeLS Auditorium and Seminar Rooms 1&amp;2 </h2>
+                    </div> -->
+                    <div class="col-lg-12">
+                        <div class="col-lg-12">
+                            <a name="Venue" id="Venue" data-target="#Venue"> </a>
+                            <h2 style="text-align: center;font-size: -webkit-xxx-large;">Mark Mattson International Neuroscience Symposium</h2>
+                            <p>
+                                <img style="height: 800px;" src="files/mattson.jpg" alt="CeLS" class="img-responsive displayed">
+                            </p>
+                            <p style="text-align: center; font-size: 26px;font-weight: bold;"> Johns Hopkins University Mountcastle Auditorium
+                                <br/> <span>June 3rd 2019</span>
+
+                            </p>
+
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4">
+                        <p> </p>
+                    </div>
+                    <div class="col-lg-12">
+                        <a name="Registration" id="Registration"> </a>
+                        <h2>REGISTRATION</h2>
+
+                        {{--<a role="button" href="https://goo.gl/forms/UP5N2tLOUnH2AHPX2" class="btn btn-danger btn-lg">Registration</a>--}}
+                        <p></p>
+
+                        <p>Registration Deadline:
+                            <b>May 1st 2019
+                            </b>
+                        </p>
+
+
+                    </div>
+
+                    <div class="col-lg-12">
+                        <a name="Speakers" id="Speakers"> </a>
+                        <h2>SPEAKERS</h2>
+                    </div>
+                    <div class="col-lg-4">
+                        <p>
+                            <a href="http://profiles.ucsd.edu/jeremy.rich">
+                                <img src="images/speakers/rich-jeremy_cr.jpg" alt="" width="100%">
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class="col-lg-12"></div>
+
+
+                    <div class="col-lg-12">
+                        <a name="Program" id="Program"> </a>
+                        <h2>SCIENTIFIC PROGRAM</h2>
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th scope="row"></th>
+                                <td>
+                                    <h3>Monday, June 3rd 2019</h3>
+                                </td>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">0800 - 0830</th>
+                                <td>
+                                    <p>
+                                        <strong>REGISTRATION</strong></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">0830 - 0845</th>
+                                <td>
+                                    <strong>Opening remarks </strong> - Thiruma Arumugam</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">0845 - 0930</th>
+                                <td>
+                                    <strong>Keynote speaker </strong>- Sol Snyder</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">0930 - 1000</th>
+                                <td>
+                                    <p>
+                                        <strong>Plenary speaker 1 </strong> - Allan Butterfield</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1000 - 1020</th>
+                                <td>
+                                    <p><strong>Speaker 1 </strong>- Alexis Stranahan
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1020 - 1040</th>
+                                <td>
+                                    <p><strong>Tea Break/ Posters</strong></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1040 - 1100</th>
+                                <td>
+                                    <p>
+                                        <strong>Speaker 2 </strong>- Justin Lathia</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1100 - 1120</th>
+                                <td>
+                                    <p><strong>Speaker 3 </strong> - Eitan Okun</p>
+                                </td>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1120 - 1140</th>
+                                <td>
+                                    <p><strong>Speaker 4</strong> - Katsutoshi Furukawa</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1140 - 1200</th>
+                                <td>
+                                    <p><strong>Speaker 5</strong> - Dong Gyu Jo</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1140 - 1220</th>
+                                <td>
+                                    <p><strong>Speaker 6</strong> - Carsten Culmsee</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1220 - 1230</th>
+                                <td>
+                                    <p><strong>Mark’s life by Roy Cutler/Simon</strong></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1230 - 1330</th>
+                                <td>
+                                    <p><strong>Lunch break and Posters</strong></p>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">1330 - 1400</th>
+                                <td>
+                                    <p><strong>Plenary Speaker 2 - Norman Haughey</strong></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1400 - 1420</th>
+                                <td>
+                                    <p><strong>Speaker 7</strong> - Wenzhen Duan</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1420 - 1440</th>
+                                <td>
+                                    <p><strong>Speaker 8</strong> - Jaewon Lee</p>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">1440 - 1500</th>
+                                <td>
+                                    <p><strong>Speaker 9</strong> - Rafa de Cabo</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1500 - 1520</th>
+                                <td>
+                                    <p><strong>Speaker 10</strong> - Steven Barger</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1520 - 1540</th>
+                                <td>
+                                    <p><strong>Coffee break/Posters</strong></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1540 - 1600</th>
+                                <td>
+                                    <p><strong>Speaker 11</strong> - Annadora Bruce-Keller</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1600 - 1620</th>
+                                <td>
+                                    <p><strong>Speaker 12</strong> - Krisztina Marosi</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1620 - 1640</th>
+                                <td>
+                                    <p><strong>Speaker 13</strong> - Benjamin Sredni (TBC)</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1640 - 1700</th>
+                                <td>
+                                    <p><strong>Speaker 14</strong> - Virginia Smith-Swintosky</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1700 - 1720</th>
+                                <td>
+                                    <p><strong>Speaker 15</strong> - Will Bohr (TBC)</p>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">1720 - 1740</th>
+                                <td>
+                                    <p><strong>Speaker 16</strong> - Kah Leong Lim</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1740 - 1800</th>
+                                <td>
+                                    <p><strong>MARK MATTSON’s Point of View (Mark Mattson)</strong></p>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">1800 - 1815</th>
+                                <td>
+                                    <p><strong> Closing Remarks </strong> - Aiwu Cheng/ Ruiqian Wan</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">1830 - 2100</th>
+                                <td> <p><strong> Gala Dinner </strong></p>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="col-lg-12"></div>
+
+
+
+                    <div class="col-lg-12">
+                        <a name="Organising" id="Organising"> </a>
+                        <h2>ORGANISING COMMITTEE</h2>
+                        <p>&nbsp; </p>
+                        <table class="table">
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <p>
+                                        <strong>Prof Norman Haughey</strong>
+                                    </p>
+                                    <p>
+                                        <strong>Prof Dong-Gyu Jo</strong>
+                                    </p>
+                                    <p>
+                                        <strong>Prof Jaewon Lee</strong>
+                                    </p>
+                                    <p>
+                                        <strong> A/Prof Wenzhen Duan</strong>
+                                    </p>
+                                </td>
+                                <td>
+                                    <p>
+                                        <strong> A/Prof Justin Lathia</strong>
+                                    </p>
+                                    <p>
+                                        <strong> Dr Aiwu Cheng</strong>
+                                    </p>
+                                    <p>
+                                        <strong> Dr Roy Cutler</strong>
+                                    </p>
+                                    <p>
+                                        <strong> Dr Ruiqian Wan</strong>
+                                    </p>
+                                </td>
+                                <td>
+                                    <p>
+                                        <strong> Dr Simonetta Camandola</strong>
+                                    </p>
+                                    <p>
+                                        <strong> A/Prof Alexis Stranahan</strong>
+                                    </p>
+                                    <p>
+                                        <strong> A/Prof Thiruma Arumugam</strong>
+                                    </p>
+                                    <p>
+                                        <strong> A/Prof Sung-Chun Tang</strong>
+                                    </p>
+
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <!-- START BASIC HOMEPAGE -->
+
+
+
+    </div>
+    <!-- NUS WRAPPER -->
+
+</div>
+<!-- NUS BODY CONTAINER -->
+
+
+</body>
+<footer></footer>
+
+</html>
